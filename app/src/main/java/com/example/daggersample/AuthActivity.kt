1daabb6ea1ccd957c67e57921d6c3c6a73a2f276
varun508 <imva508@gmail.com>
2019-07-20 08:52:22 +0530

    Add AuthActivity, mark it as launcher

---
+++ app/src/main/java/com/example/daggersample/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.daggersample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
